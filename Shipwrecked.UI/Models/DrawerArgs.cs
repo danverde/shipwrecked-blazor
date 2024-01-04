1953a2e28cb6be5cceec33af3c05c54e3b93b3a0
danverde <verde.daniel.d@gmail.com>
2024-01-03 20:46:24 -0500

    - add additional XML comments

--- Shipwrecked.UI/Models/DrawerArgs.cs
+++ Shipwrecked.UI/Models/DrawerArgs.cs
@@ -1,15 +1,26 @@
 namespace Shipwrecked.UI.Models;
 
+/// <summary>
+/// Represents the payload of a DrawerToggled Event
+/// </summary>
 public class DrawerArgs : EventArgs
 {
+    /// <summary>
+    /// Constructor specifying all dependencies
+    /// </summary>
     public DrawerArgs(DrawerId drawerId, bool isOpen)
     {
         Id = drawerId;
         IsOpen = isOpen;
     }
 
-    // public string Message { get; set; }
-    
+    /// <summary>
+    /// The unique Id of the drawer
+    /// </summary>
     public DrawerId Id { get; set; }
+    
+    /// <summary>
+    /// Determines if the Drawer is open or closed
+    /// </summary>
     public bool IsOpen { get; set; }
 }
--- Shipwrecked.UI/Models/DrawerId.cs
+++ Shipwrecked.UI/Models/DrawerId.cs
@@ -1,5 +1,8 @@
 namespace Shipwrecked.UI.Models;
 
+/// <summary>
+/// Enum specifying the various drawer Ids
+/// </summary>
 public enum DrawerId
 {
     HelpMenu
