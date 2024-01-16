39c95109113e8f3a147f7605a5a051df2bfd5c2e
danverde <verde.daniel.d@gmail.com>
2024-01-15 19:46:29 -0500

    - load screen polished

--- Shipwrecked.UI/Components/Common/Button.razor.cs
+++ Shipwrecked.UI/Components/Common/Button.razor.cs
@@ -19,50 +19,44 @@ public partial class Button
     /// <summary>
     /// Text content of the button
     /// </summary>
-    [Parameter]
-    public RenderFragment? ChildContent { get; set; }
+    [Parameter] public RenderFragment? ChildContent { get; set; }
 
+    [Parameter] public string Styles { get; set; } = "";
+    
     /// <summary>
     /// Corresponds with the HTML button type attribute
     /// </summary>
-    [Parameter]
-    public string Type { get; set; } = "button";
+    [Parameter] public string Type { get; set; } = "button";
     
     /// <summary>
     /// Corresponds with the HTML button value attribute
     /// </summary>
-    [Parameter]
-    public string Value { get; set; } = "";
+    [Parameter] public string Value { get; set; } = "";
     
     /// <summary>
     /// Corresponds with the Form button attribute 
     /// </summary>
-    [Parameter]
-    public string FormId { get; set; } = "";
+    [Parameter] public string FormId { get; set; } = "";
     
     /// <summary>
     /// Disables the button
     /// </summary>
-    [Parameter]
-    public bool Disabled { get; set; }
+    [Parameter] public bool Disabled { get; set; }
 
     /// <summary>
     /// The type of the button. Changes the styling
     /// </summary>
-    [Parameter]
-    public ButtonType Variant { get; set; } = ButtonType.Primary;
+    [Parameter] public ButtonType Variant { get; set; } = ButtonType.Primary;
     
     /// <summary>
     /// The location to navigate to on button click
     /// </summary>
-    [Parameter]
-    public string? Route { get; set; }
+    [Parameter] public string? Route { get; set; }
 
     /// <summary>
     /// Callback letting parent component know the onClick event has been triggered
     /// </summary>
-    [Parameter]
-    public EventCallback OnClickCallback { get; set; }
+    [Parameter] public EventCallback OnClickCallback { get; set; }
 
     #endregion
 
